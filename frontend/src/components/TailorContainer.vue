5a388d1ec22adc4d4f6cf9fe43b6b1e7a1f46486
Elias Jaffe <elijaffe173@gmail.com>
2024-03-04 01:29:36 -0500

    add api call to tfidf

--- frontend/src/components/TailorContainer.vue
+++ frontend/src/components/TailorContainer.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="rounded-light-spacing">
     <h2>Results</h2>
-    <ol v-for="job in resume" :key="job.id">
+    <button @click="getAnalytics()">Run Results!</button>
+    <div v-if="currentState==='unloaded'">
+      <p>No analytics yet.</p>
+    </div>
+    <div v-else-if="currentState==='loading'">
+      <p>Analytics Loading...</p>
+    </div>
+    <div v-else-if="currentState==='error'">
+      <p>Issue! Try again later.</p>
+    </div>
+    <div v-else-if="currentState==='loaded'">
+      <ol v-for="job in enriched.resume" :key="job.id">
       <li>
         {{ job.title }}
         <ol v-for="bullet in job.bullets" :key="bullet.id">
           <li>
             {{ bullet.text }}
-            <ol v-for="req in requirements" :key="req.id">
-              <li>{{ req.value }}</li>
+            <ol v-for="distance in distances" :key="distance.req_id">
+              <li>{{ distance }}</li>
             </ol>
           </li>
         </ol>
       </li>
     </ol>
+    </div>
   </div>
 </template>
 
@@ -26,15 +38,38 @@ import { useRequirements } from '../store/requirements.ts';
 export default defineComponent({
   name: 'TailorContainer',
   components: {},
+  methods: {
+    getAnalytics() {
+      try {
+        this.currentState = 'loading';
+        this.enriched = this.$http.post(
+          '/enrich/tfidf/',
+          {
+            resume: this.resume,
+            requirements: this.requirements,
+          },
+        );
+        this.currentState = 'loaded';
+      } catch {
+        this.currentState = 'error'
+      }
+    },
+  },
   setup() {
     const resumeStore = useResume();
     const { resume } = storeToRefs(resumeStore);
     const requirementsStore = useRequirements();
     const { requirements } = storeToRefs(requirementsStore);
-
+    let currentState: string = 'unloaded';
+    let enriched = {
+      resume: [],
+      requirements: [],
+    };
     return {
+      currentState: currentState,
       resume: resume,
       requirements: requirements,
+      enriched: enriched,
     };
   },
 });
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './style.css';
 import App from './App.vue';
 
-createApp(App).use(createPinia()).mount('#app');
+createApp(
+    App
+).use(
+    createPinia()
+).use(
+    VueAxios, axios
+).mount('#app');
